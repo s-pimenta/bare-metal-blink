b5ba5246598e5e33d9ae1d727820a9f28957c54b
S Pimenta <sergiobmpimenta@gmail.com>
2025-08-05 19:03:38 +0100

    Optimized code for using the GPIO_OUT_XOR register

--- bare-metal-blink.c
+++ bare-metal-blink.c
@@ -2,10 +2,8 @@
 int main() {
     *(volatile uint32_t*)0x400140cc = 0x5; // GPIO 25 ctrl (SIO function)
     *(volatile uint32_t*)0xd0000020 = 0x2000000; // Enable output on GPIO 25
-    while (1) {
+    while (1) { //loop forever
         sleep_ms(1000); // pause 1 second
-        *(volatile uint32_t*)0xd0000014 = 0x2000000;  // Set high
-        sleep_ms(1000); // pause 1 second
-        *(volatile uint32_t*)0xd0000018 = 0x2000000;  // Set low
+        *(volatile uint32_t*)0xd000001c = 0x2000000;  // Toggle LED
     }   
 }

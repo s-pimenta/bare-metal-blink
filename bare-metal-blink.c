6d1e9d8bca9282bfaacd3d25c915dd348592b021
S Pimenta <sergiobmpimenta@gmail.com>
2025-08-07 17:13:15 +0100

    Bitshifting by using the preprocessor

--- bare-metal-blink.c
+++ bare-metal-blink.c
@@ -1,9 +1,12 @@
 #include "pico/stdlib.h"
+#define GPIO25 (1u << 25) //GPIO25
+
 int main() {
-    *(volatile uint32_t*)0x400140cc = 0x5; // GPIO 25 ctrl (SIO function)
-    *(volatile uint32_t*)0xd0000020 = 1 << 25; //Enable output GPIO25 by left-shifting 1, 25 times
+    *(volatile uint32_t*)0x400140cc = 0x5;// GPIO 25 ctrl (SIO function)
+    *(volatile uint32_t*)0xd0000020 = GPIO25; //Enable output GPIO25
+    
     while (1) { //loop forever
         sleep_ms(1000); // pause 1 second
-        *(volatile uint32_t*)0xd000001c = 1 << 25;  // Toggle LED by left-shifting 1, 25 times
+        *(volatile uint32_t*)0xd000001c = GPIO25; //Toggle LED
     }   
 }

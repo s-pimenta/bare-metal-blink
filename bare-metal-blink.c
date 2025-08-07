3055cd53fca384fa00dd6310ab1b536d9e5c0ba6
S Pimenta <sergiobmpimenta@gmail.com>
2025-08-07 16:36:37 +0100

    No brackets needed for bitshifting

--- bare-metal-blink.c
+++ bare-metal-blink.c
@@ -1,9 +1,9 @@
 #include "pico/stdlib.h"
 int main() {
     *(volatile uint32_t*)0x400140cc = 0x5; // GPIO 25 ctrl (SIO function)
-    *(volatile uint32_t*)0xd0000020 = (1 << 25); //Enable output GPIO25 by left-shifting 1, 25 times
+    *(volatile uint32_t*)0xd0000020 = 1 << 25; //Enable output GPIO25 by left-shifting 1, 25 times
     while (1) { //loop forever
         sleep_ms(1000); // pause 1 second
-        *(volatile uint32_t*)0xd000001c = (1 << 25);  // Toggle LED by left-shifting 1, 25 times
+        *(volatile uint32_t*)0xd000001c = 1 << 25;  // Toggle LED by left-shifting 1, 25 times
     }   
 }
